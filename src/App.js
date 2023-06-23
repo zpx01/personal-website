2acd0f5c258cee27fe3051e9b92f2f8e6228fec1
Zeeshan Patel <zeeshanp@berkeley.edu>
2023-06-23 12:12:25 -0700

    initial research website complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { Navbar } from "./components/Navbar";
 import { Main } from "./components/Main";
+import Research from './components/Research'; // Assuming Research.js is in the same location as your other components
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <ChakraProvider>
+    <ChakraProvider>
+      <Router>
         <Navbar />
-        <Main />
-      </ChakraProvider>
-    </>
+        <Switch>
+          <Route path="/research" component={Research} />
+          <Route path="/" component={Main} />
+        </Switch>
+      </Router>
+    </ChakraProvider>
   );
 }
 
---
+++ src/components/Research.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Research extends React.Component {
+    render(){
+        return (
+            <div>
+                <object type="text/html" data="/research/index.html" width="100%" height="100%" style={{overflow: 'auto', display: 'flex', flexDirection: 'column'}}></object>
+            </div>
+        );
+    }
+}
+
+export default Research;
