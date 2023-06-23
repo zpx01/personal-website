98028777a38f2764999bcacfd3f5862d7c2d6179
Zeeshan Patel <zeeshanp@berkeley.edu>
2023-06-23 14:20:26 -0700

    added aria label

--- src/components/Research.js
+++ src/components/Research.js
@@ -4,7 +4,7 @@ class Research extends React.Component {
     render(){
         return (
             <div>
-                <object type="text/html" data="/research/index.html" width="100%" height="100%" style={{overflow: 'auto', display: 'flex', flexDirection: 'column'}}></object>
+                <object aria-label="Research Page" type="text/html" data="/research/index.html" width="100%" height="100%" style={{overflow: 'auto', display: 'flex', flexDirection: 'column'}}></object>
             </div>
         );
     }
